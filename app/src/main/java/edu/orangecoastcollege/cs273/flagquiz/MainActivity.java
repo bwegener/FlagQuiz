573d45eaae837a630b1930546271f115eb098d1f
bwegener <wegener_brian@yahoo.com>
2017-10-05 16:08:55 -0700

    Commit 4:10pm 10.5.17

--- app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/MainActivity.java
@@ -1,17 +1,24 @@
 package edu.orangecoastcollege.cs273.flagquiz;
 
 import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static edu.orangecoastcollege.cs273.flagquiz.R.id.questionNumberTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,12 +48,34 @@ public class MainActivity extends AppCompatActivity {
         rng = new SecureRandom();
         handler = new Handler();
 
-        // TODO: Get references to GUI components (textviews and imageview)
-        // TODO: Put all 4 buttons in the array (mButtons)
-        // TODO: Set mQuestionNumberTextView's text to the appropriate strings.xml resource
-        // TODO: Load all the countries from the JSON file using the JSONLoader
-        // TODO: Call the method resetQuiz() to start the quiz.
+        // COMPLETED: Get references to GUI components (textviews and imageview)
+        mQuestionNumberTextView = (TextView) findViewById(questionNumberTextView);
+        mFlagImageView = (ImageView) findViewById(R.id.flagImageView);
+        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+        TextView mGuessCountryTextView = (TextView) findViewById(R.id.guessCountryTextView);
 
+        // COMPLETED: Put all 4 buttons in the array (mButtons)
+        mButtons[0] = (Button) findViewById(R.id.button);
+        mButtons[1] = (Button) findViewById(R.id.button2);
+        mButtons[2] = (Button) findViewById(R.id.button3);
+        mButtons[3] = (Button) findViewById(R.id.button4);
+
+        // COMPLETED: Set mQuestionNumberTextView's text to the appropriate strings.xml resource
+        // This sets both arguments the first question == 1, the total number is constant FLAGS_IN_QUIZ
+        mQuestionNumberTextView.setText(getString(R.string.question, 1, FLAGS_IN_QUIZ));
+
+        // COMPLETED: Load all the countries from the JSON file using the JSONLoader
+        try {
+            mAllCountriesList = JSONLoader.loadJSONFromAsset(this);
+        }
+
+        catch (IOException e)
+        {
+            Log.e("Flag Quiz", "Error loading from JSON", e);
+        }
+
+        // COMPLETED: Call the method resetQuiz() to start the quiz.
+        resetQuiz();
     }
 
     /**
@@ -54,14 +83,35 @@ public class MainActivity extends AppCompatActivity {
      */
     public void resetQuiz() {
 
-        // TODO: Reset the number of correct guesses made
-        // TODO: Reset the total number of guesses the user made
-        // TODO: Clear list of quiz countries (for prior games played)
+        // COMPLETED: Reset the number of correct guesses made
+        mCorrectGuesses = 0;
 
-        // TODO: Randomly add FLAGS_IN_QUIZ (10) countries from the mAllCountriesList into the mQuizCountriesList
-        // TODO: Ensure no duplicate countries (e.g. don't add a country if it's already in mQuizCountriesList)
+        // COMPLETED: Reset the total number of guesses the user made
+        mTotalGuesses = 0;
 
-        // TODO: Start the quiz by calling loadNextFlag
+        // COMPLETED: Clear list of quiz countries (for prior games played)
+        mQuizCountriesList.clear();
+
+        // COMPLETED: Randomly add FLAGS_IN_QUIZ (10) countries from the mAllCountriesList into the mQuizCountriesList
+        // COMPLETED: Ensure no duplicate countries (e.g. don't add a country if it's already in mQuizCountriesList)
+        int size = mAllCountriesList.size();
+        int randomPosition;
+
+        while(mQuizCountriesList.size() < FLAGS_IN_QUIZ)
+        {
+            // Generate random position:
+            randomPosition = rng.nextInt(size);
+            Country randomCountry = mAllCountriesList.get(randomPosition);
+
+            // Determine whether to add the random country
+            if(!mQuizCountriesList.contains(randomCountry))
+            {
+                mQuizCountriesList.add(randomCountry);
+            }
+        }
+
+        // COMPLETED: Start the quiz by calling loadNextFlag
+        loadNextFlag();
     }
 
     /**
@@ -69,23 +119,38 @@ public class MainActivity extends AppCompatActivity {
      * the flag's image and then 4 buttons, one of which contains the correct answer.
      */
     private void loadNextFlag() {
-        // TODO: Initialize the mCorrectCountry by removing the item at position 0 in the mQuizCountries
-        // TODO: Clear the mAnswerTextView so that it doesn't show text from the previous question
-        // TODO: Display current question number in the mQuestionNumberTextView
 
+        // COMPLETED: Initialize the mCorrectCountry by removing the item at position 0 in the mQuizCountries
+        mCorrectCountry = mQuizCountriesList.remove(0);
 
-        // TODO: Use AssetManager to load next image from assets folder
+        // COMPLETED: Clear the mAnswerTextView so that it doesn't show text from the previous question
+        mAnswerTextView.setText("");
+
+        // COMPLETED: Display current question number in the mQuestionNumberTextView
+        int questionNumber = FLAGS_IN_QUIZ - mQuizCountriesList.size();
+        mQuestionNumberTextView.setText(getString(R.string.question, questionNumber, FLAGS_IN_QUIZ));
+
+        // COMPLETED: Use AssetManager to load next image from assets folder
         AssetManager am = getAssets();
 
-        // TODO: Get an InputStream to the asset representing the next flag
-        // TODO: and try to use the InputStream to create a Drawable
-        // TODO: The file name can be retrieved from the correct country's file name.
-        // TODO: Set the image drawable to the correct flag.
+        // COMPLETED: Get an InputStream to the asset representing the next flag
+        // COMPLETED: and try to use the InputStream to create a Drawable
+        // COMPLETED: The file name can be retrieved from the correct country's file name.
+        // COMPLETED: Set the image drawable to the correct flag.
+        try {
+            InputStream stream = am.open(mCorrectCountry.getFileName());
+            Drawable image = Drawable.createFromStream(stream, mCorrectCountry.getName());
+            mFlagImageView.setImageDrawable(image);
+        } catch (IOException e) {
+            Log.e(TAG, "Error loading image" + mCorrectCountry.getFileName(), e);
+        }
 
-        // TODO: Shuffle the order of all the countries (use Collections.shuffle)
+        // COMPLETED: Shuffle the order of all the countries (use Collections.shuffle)
+        Collections.shuffle(mAllCountriesList);
 
         // TODO: Loop through all 4 buttons, enable them all and set them to the first 4 countries
         // TODO: in the all countries list
+        int buttonSize =
 
 
         // TODO: After the loop, randomly replace one of the 4 buttons with the name of the correct country
@@ -101,11 +166,13 @@ public class MainActivity extends AppCompatActivity {
      */
     public void makeGuess(View v) {
         // TODO: Downcast the View v into a Button (since it's one of the 4 buttons)
-        // TODO: Get the country's name from the text of the button
 
+        // TODO: Get the country's name from the text of the button
         // TODO: If the guess matches the correct country's name, increment the number of correct guesses,
         // TODO: then display correct answer in green text.  Also, disable all 4 buttons (can't keep guessing once it's correct)
+
         // TODO: Nested in this decision, if the user has completed all 10 questions, show an AlertDialog
+
         // TODO: with the statistics and an option to Reset Quiz
 
         // TODO: Else, the answer is incorrect, so display "Incorrect Guess!" in red
